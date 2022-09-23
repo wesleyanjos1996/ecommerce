e76f6c49369a7816bc05df85510a78c40795a3f0
Wesley dos Anjos <wesleyanjosprogramming@outlook.com.br>
2022-09-23 20:23:35 -0300

    Adicionado admin views/admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 //$app = new \Slim\Slim();
 $app = new Slim();
@@ -21,6 +22,11 @@ $app->get('/', function() {
 	$page = new Page();
 	$page->setTpl("index");
 
+});
+
+$app->get("/admin", function(){
+	$page = new PageAdmin();
+	$page->setTpl("index");
 });
 
 $app->run();
