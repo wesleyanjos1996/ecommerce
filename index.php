c82c6804641add735c7ceb05dc130159a6abf1d8
Wesley dos Anjos <wesleyanjosprogramming@outlook.com.br>
2022-09-23 12:25:32 -0300

    criado page class

--- index.php
+++ index.php
@@ -2,16 +2,24 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+//$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
 	//echo "OK";
+	/*
 	$sql = new \Hcode\DB\Sql();
 	$results = $sql->select("SELECT * FROM tb_users");
 	echo json_encode($results);
+	*/
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
